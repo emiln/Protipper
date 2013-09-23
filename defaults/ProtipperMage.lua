bf82d0d9284dabefade2f86751ba6b70e362384b
Mikael Kragbæk <mikael@kragbaek.com>
2013-09-24 00:09:48 +0200

    Started working on the fire mage priority list and fixed indentation.
    Still a lot of work to do. Combustion is a tricky one and pyroblast
    currently only shows in the most general cases where it should be cast.

--- defaults/ProtipperMage.lua
+++ defaults/ProtipperMage.lua
@@ -7,18 +7,18 @@ Protipper.COOLDOWN_FREE_SPELL["Arcane"] = "Arcane Blast";
 Protipper.SPEC_LIST["Arcane"] = {
    preparation = {
       { "Arcane Brilliance",
-	[[not (p.BuffActive('Dark Intent', 'player') or
-    p.BuffActive('Arcane Brilliance', 'player') or
-    p.BuffActive('Dalaran Brilliance', 'player') or
-    p.BuffActive('Burning Wrath', 'player') or
-    p.BuffActive('Still Water', 'player'))]] },
+        [[not (p.BuffActive('Dark Intent', 'player') or
+               p.BuffActive('Arcane Brilliance', 'player') or
+               p.BuffActive('Dalaran Brilliance', 'player') or
+               p.BuffActive('Burning Wrath', 'player') or
+               p.BuffActive('Still Water', 'player'))]] },
       
       { "Mage Armor",
-	"not p.BuffActive('Mage Armor', 'player')" }
+        "not p.BuffActive('Mage Armor', 'player')" }
    },
    default = {
       { "Arcane Blast",
-	"true" }
+        "true" }
    }
 };
 
@@ -28,49 +28,51 @@ Protipper.SPEC_LIST["Fire"] = {
 
    preparation = {
       { "Arcane Brilliance",
-	[[not (p.BuffActive('Dark Intent', 'player') or
-    p.BuffActive('Arcane Brilliance', 'player') or
-    p.BuffActive('Dalaran Brilliance', 'player') or
-    p.BuffActive('Burning Wrath', 'player') or
-    p.BuffActive('Still Water', 'player'))]] },
+        [[not (p.BuffActive('Dark Intent', 'player') or
+               p.BuffActive('Arcane Brilliance', 'player') or
+               p.BuffActive('Dalaran Brilliance', 'player') or
+               p.BuffActive('Burning Wrath', 'player') or
+               p.BuffActive('Still Water', 'player'))]] },
       
       { "Molten Armor",
-	"not p.BuffActive('Molten Armor', 'player')" }
+        "not p.BuffActive('Molten Armor', 'player')" }
    },
 
    default = {      
       { "Mirror Image",
-	"p.AbilityReady('Mirror Image')" },
+        "p.AbilityReady('Mirror Image')" },
       
       { "Presence of Mind",
-	[[p.TalentActive('Presence of Mind') and
-       p.AbilityReady('Presence of Mind')]]},
+        [[p.TalentActive('Presence of Mind') and
+          p.AbilityReady('Presence of Mind')]]},
       
       { "Pyroblast",
-	[[p.BuffActive('Pyroblast!', 'player') or 
-       p.BuffActive('Presence of Mind', 'player')]] },
+        [[(p.BuffActive('Pyroblast!', 'player') and 
+           p.BuffActive('Heating Up', 'player') and
+           p.IsCasting('Fireball')) or 
+          p.BuffActive('Presence of Mind', 'player')]] },
       
       { "Inferno Blast",
-	[[p.BuffActive('Heating Up', 'player') and
-       p.AbilityReady('Inferno Blast')]] },
+        [[p.BuffActive('Heating Up', 'player') and
+          p.AbilityReady('Inferno Blast')]] },
       
       { "Nether Tempest",
-	[[p.TalentActive('Nether Tempest') and 
-       p.DebuffRefresh('Nether Tempest') and
-       p.AbilityReady('Nether Tempest')]]},
+        [[p.TalentActive('Nether Tempest') and 
+          p.DebuffRefresh('Nether Tempest') and
+          p.AbilityReady('Nether Tempest')]]},
       
       { "Frost Bomb",
-	[[p.TalentActive('Frost Bomb') and 
-       p.DebuffRefresh('Frost Bomb') and
-       p.AbilityReady('Frost Bomb')]]},
+        [[p.TalentActive('Frost Bomb') and 
+          p.DebuffRefresh('Frost Bomb') and
+          p.AbilityReady('Frost Bomb')]]},
       
       { "Living Bomb",
-	[[p.TalentActive('Living Bomb') and 
-       p.DebuffRefresh('Living Bomb') and
-       p.AbilityReady('Living Bomb')]]},
+        [[p.TalentActive('Living Bomb') and 
+          p.DebuffRefresh('Living Bomb') and
+          p.AbilityReady('Living Bomb')]]},
       
       { "Fireball",
-	"true" }
+        "true" }
    }
 };
 
@@ -80,62 +82,62 @@ Protipper.SPEC_LIST["Frost"] = {
 
    preparation = {
       { "Arcane Brilliance",
-	[[not (p.BuffActive('Dark Intent', 'player') or
-       p.BuffActive('Arcane Brilliance', 'player') or
-       p.BuffActive('Dalaran Brilliance', 'player') or
-       p.BuffActive('Burning Wrath', 'player') or
-       p.BuffActive('Still Water', 'player'))]] },
+        [[not (p.BuffActive('Dark Intent', 'player') or
+               p.BuffActive('Arcane Brilliance', 'player') or
+               p.BuffActive('Dalaran Brilliance', 'player') or
+               p.BuffActive('Burning Wrath', 'player') or
+               p.BuffActive('Still Water', 'player'))]] },
       
       { "Frost Armor",
-	"not p.BuffActive('Frost Armor', 'player')" },
+        "not p.BuffActive('Frost Armor', 'player')" },
       
       { "Summon Water Elemental",
-	[[not p.PetActive() and 
-        p.AbilityReady('Summon Water Elemental')]] }
+        [[not p.PetActive() and 
+          p.AbilityReady('Summon Water Elemental')]] }
    },
 
    default = {
       { "Mirror Image",
-	"p.AbilityReady('Mirror Image')"},
+        "p.AbilityReady('Mirror Image')"},
       
       { "Icy Veins",
-	"p.AbilityReady('Icy Veins')" },
+        "p.AbilityReady('Icy Veins')" },
       
       { "Nether Tempest",
-	[[p.TalentActive('Nether Tempest') and 
-	  p.DebuffRefresh('Nether Tempest') and
-	  p.AbilityReady('Nether Tempest')]]},
+        [[p.TalentActive('Nether Tempest') and 
+          p.DebuffRefresh('Nether Tempest') and
+          p.AbilityReady('Nether Tempest')]]},
       
       { "Frost Bomb",
-	[[p.TalentActive('Frost Bomb') and 
-	  p.DebuffRefresh('Frost Bomb') and
-	  p.AbilityReady('Frost Bomb')]]},
+        [[p.TalentActive('Frost Bomb') and 
+          p.DebuffRefresh('Frost Bomb') and
+          p.AbilityReady('Frost Bomb')]]},
       
       { "Living Bomb",
-	[[p.TalentActive('Living Bomb') and 
-	  p.DebuffRefresh('Living Bomb') and
-	  p.AbilityReady('Living Bomb')]]},
+        [[p.TalentActive('Living Bomb') and 
+          p.DebuffRefresh('Living Bomb') and
+          p.AbilityReady('Living Bomb')]]},
       
       { "Frozen Orb",
-	"p.AbilityReady('Frozen Orb')" },
+        "p.AbilityReady('Frozen Orb')" },
       
       { "Frostbolt",
-	"p.DebuffRefresh('Frostbolt')" },
+        "p.DebuffRefresh('Frostbolt')" },
       
       { "Freeze",
-	[[not p.BuffStack('Fingers of Frost', 2, 2, 'player') and
-	  p.PetActive() and 
-	  p.PetAbilityReady('Freeze')]] },
+        [[not p.BuffStack('Fingers of Frost', 2, 2, 'player') and
+          p.PetActive() and 
+          p.PetAbilityReady('Freeze')]] },
       
       { "Frostfire Bolt",
-	[[p.BuffActive('Brain Freeze', 'player') and
-	  p.AbilityReady('Frostfire Bolt')]] },
+        [[p.BuffActive('Brain Freeze', 'player') and
+          p.AbilityReady('Frostfire Bolt')]] },
       
       { "Ice Lance",
-	[[p.BuffActive('Fingers of Frost', 'player') and
-	  p.AbilityReady('Ice Lance')]] },
+        [[p.BuffActive('Fingers of Frost', 'player') and
+          p.AbilityReady('Ice Lance')]] },
       
       { "Frostbolt",
-	"true" }
+        "true" }
    }
 };
