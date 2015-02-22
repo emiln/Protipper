6d4117d634a9c483dfc29f6695a5276333e138f4
Mikael Kragbæk <mikael@kragbaek.com>
2015-02-22 18:05:55 +0100

    Set up a basic arcane mage rotation taking into account both burn and conserve phases. Absolutely ignores buff-management and using mage talents.

--- defaults/ProtipperMage.lua
+++ defaults/ProtipperMage.lua
@@ -5,15 +5,51 @@ end
 Protipper.COOLDOWN_FREE_SPELL["Arcane"] = "Arcane Blast"
 
 Protipper.SPEC_LIST["Arcane"] = {
-   preparation = {},
-   default = {
-      {
-        "Arcane Blast",
-        function(api)
-          return true
-        end
-      }
-   }
+  preparation = {},
+  default = {
+    {
+      "Arcane Power",
+      function(api)
+        return api.Spell("Evocation").remainingCooldown <= 30 and api.Spell("Arcane Power").isReady
+      end
+    },
+    {
+      "Evocation",
+      function(api)
+        return api.Helpers.PowerPercent("player") <= 50 and api.Spell("Evocation").isReady
+      end
+    },
+    {
+      "Arcane Missiles",
+      function(api)
+        return (api.Effect("Arcane Missiles!", "player").stacks >= 3 and api.Helpers.PowerPercent("player") > 70)
+      end
+    },
+    {
+      "Arcane Blast",
+      function(api)
+        return api.Helpers.PowerPercent("player") > 93
+      end
+    },
+    {
+      "Arcane Missiles",
+      function(api)
+        return api.Effect("Arcane Charge", "player").stacks >= 4 and api.Effect("Arcane Missiles!", "player").isActive and api.Helpers.PowerPercent("player") > 70
+      end
+    },
+    {
+      "Arcane Barrage",
+      function(api)
+        return api.Effect("Arcane Charge", "player").stacks >= 4 and api.Spell("Evocation", "player").remainingCooldown > 30
+      end
+    },
+    {
+      "Arcane Blast",
+      function(api)
+        return true
+      end
+    }
+  }
 }
 
 Protipper.COOLDOWN_FREE_SPELL["Fire"] = "Fireball"
